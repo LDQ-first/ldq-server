dc2a5f969d80a6dc65000e247ff118f3acbc59b4
LDQ-first <178848890@qq.com>
2018-02-11 20:53:52 +0800

    cli

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # node-anydoor
+
+## Install
+
+```
+npm i -g node-anydoor
+```
+## Use
+
+```
+node-anydoor # 把当前文件夹作为静态资源服务器根目录
+
+node-anydoor -p 8080 # 设置端口号为 8080
+
+node-anydoor -h localhost # 设置 host 为 localhost
+
+node-anydoor -d /usr # 设置根目录为 /usr
+```
--- src/app.js
+++ src/app.js
@@ -5,13 +5,23 @@ const path = require('path')
 const conf = require('./config/defaultConfig')
 const route = require('./helper/route')
 
-const server = http.createServer((req, res) => {
-  const url = req.url
-  const filePath = path.join(conf.root, url)
-  route(req, res, filePath)
-})
+class Server {
+  constructor (config) {
+    this.conf = Object.assign({}, conf, config)
+  }
 
-server.listen(conf.port, conf.hostname, () => {
-  const addr = `http://${conf.hostname}:${conf.port}`
-  console.info(`Server started at ${chalk.green(addr)}`)
-})
+  start () {
+    const server = http.createServer((req, res) => {
+      const url = req.url
+      const filePath = path.join(this.conf.root, url)
+      route(req, res, filePath, this.conf)
+    })
+    
+    server.listen(this.conf.port, this.conf.hostname, () => {
+      const addr = `http://${this.conf.hostname}:${this.conf.port}`
+      console.info(`Server started at ${chalk.green(addr)}`)
+    })
+  }
+}
+
+module.exports = Server
